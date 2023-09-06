ca33cd7ccd707ca4f19a8590b1aeb983cda72036
devon4329 <devon4329@proton.me>
2023-09-05 18:13:26 -0600

    Guided Exploration 2
    Code done for guided exploration 2
    
    End of part 2

--- CS2060ClassCodeFall2023-main/examples/ch02/fig02_01.c
+++ CS2060ClassCodeFall2023-main/examples/ch02/fig02_01.c
@@ -4,7 +4,53 @@
 
 // function main begins program execution 
 int main(void) {
-   printf("Welcome to C!\n");
+    
+    //int is the type of the variable that will be stored in the stack
+    //length, width, and area are the names of the variables and is how
+    //the program will be able to manipulate the data.
+    // "=" is a binary operator which assigns each variable a value. If not used,
+    // the variables are only declared.
+    // 0 is the value used to initialize each variable.
+    int length = 0;
+    int width = 0;
+    int area = 0;
+    
+    //Create a variable to store the scanf return value
+    unsigned scanfReturn;
+    
+    //Prompt for length and store in variable length
+    //puts() used to securely prompt user for a length
+    //scanf() recieves information from the user
+    //%d is the format specifier for the desired input from the user
+    //& is the address operator used to tell the compiler which address to send
+    // recieved input to.
+    puts("Enter the length: ");
+    
+    //Assign scanfReturn with incorrect input.
+    scanf("%d", &length);
+    
+    
+    //Prompt for width and store in variable width
+    printf("%s", "Enter the width: ");
+    scanf("%d", &width);
+    
+    //Calcuate the area and assign it to the area variable
+    // = is used to assgin the result calculation to the area variable
+    // * is the arithmetic operator for multiplying the two variables
+    area = length * width;
+    
+    //Print out the information.
+    printf("Length: %d  Width: %d  Area: %d\n", length, width, area);
+    
+    
+    
+    printf("Welcome to C!\n");
+    
+    //Add correct return statement at end of main function
+    //Main function is supposed to return an int
+    //"0" indicates program ran successfully
+    return 0;
+    
 } // end function main 
 
 
