028f10758e62d340626df2b31512c0c6488d8b1b
devon4329 <devon4329@proton.me>
2023-11-12 16:20:42 -0700

    Create WhiteDevonGE07.c
    GE07 code created.
    Beginning prompt entered

---
+++ CS2060ClassCodeFall2023-main/examples/WhiteDevonGE07.c
@@ -0,0 +1,18 @@
+/*
+ * Name: Devon White
+ * Class: CS2060 M/W 10:50AM - 12:05PM
+ * MAC OS
+ * Due: Nov 13, 2023
+ * Description: This program will allow the user to enter pet names
+ * and ages where memory will be allocated and they will be stored
+ * alphabetically in a linked list.  After pets are enetered, the
+ * user can remove the pet and memory will be deallocated.  Before
+ * the program ends all the memory will be deallocated for any
+ * remaining pets.
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+
+
