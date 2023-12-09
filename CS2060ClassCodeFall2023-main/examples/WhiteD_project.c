7b18f51f9cdb37a20522f053124db072446db6e0
devon4329 <devon4329@proton.me>
2023-12-09 11:26:18 -0700

    Update WhiteD_project.c
    Undid refactor of pringPropertyInfo method

--- CS2060ClassCodeFall2023-main/examples/WhiteD_project.c
+++ CS2060ClassCodeFall2023-main/examples/WhiteD_project.c
@@ -74,7 +74,7 @@ typedef struct Property {
 
 
 // Function Prototypes
-void printRetnalPropertyInfo(Property currentPropPtr);
+void printRetnalPropertyInfo(Property *currentPropPtr);
 void setUpProperty(int minNights, int maxNights, int minRate, int maxRate, Property** propertyPtr);
 int getValidInt(unsigned int min, unsigned int max);
 int scanInt(char* str);
@@ -124,14 +124,14 @@ int main (void){
 // property structure.
 // Receives a property object
 // Returns void because method is printing information.
-void printRetnalPropertyInfo(Property currentPropPtr)
+void printRetnalPropertyInfo(Property *currentPropPtr)
 {
-    printf("Rental Property: %s\n", currentPropPtr.name);
-    printf("Location: %s\n", currentPropPtr.location);
+    printf("Rental Property: %s\n", currentPropPtr->name);
+    printf("Location: %s\n", currentPropPtr->location);
     printf("Rental Property can be rented for %d to %d nights.\n", MIN_RENTAL_NIGHTS, MAX_RENTAL_NIGHTS);
-    printf("$%.2f rate a night for the first %d nights.\n", currentPropPtr.rate, currentPropPtr.interval1);
-    printf("$%.2f discount rate a night for nights %d to %d\n", currentPropPtr.discount, (currentPropPtr.interval1 + 1), currentPropPtr.interval2);
-    printf("$%.2f discount rate a night for each remaining night over %d.\n\n", (currentPropPtr.discount * DISCOUNT_MULTIPLIER), currentPropPtr.interval2);
+    printf("$%.2f rate a night for the first %d nights.\n", currentPropPtr->rate, currentPropPtr->interval1);
+    printf("$%.2f discount rate a night for nights %d to %d\n", currentPropPtr->discount, (currentPropPtr->interval1 + 1), currentPropPtr->interval2);
+    printf("$%.2f discount rate a night for each remaining night over %d.\n\n", (currentPropPtr->discount * DISCOUNT_MULTIPLIER), currentPropPtr->interval2);
 } //printRentalPropertyInfo
 
 
@@ -444,7 +444,7 @@ void rentalMode(Property *currentPropPtr)
             while (currentListPtr != NULL)
             {
                 // Task 3.1 - Display rental property information and Ratings
-                printRetnalPropertyInfo(*currentListPtr);
+                printRetnalPropertyInfo(currentListPtr);
                 if (currentListPtr->ratingsEntered <= VACATION_RENTERS)
                 {
                     printSurveyResults(currentListPtr);
